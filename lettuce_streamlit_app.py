ca86386226ac4b7110bc49c2ee6edccc299cf222
surgyhoons <surgyhoon@gmail.com>
2026-03-24 02:22:22 +0900

    Update lettuce_streamlit_app.py

--- lettuce_streamlit_app.py
+++ lettuce_streamlit_app.py
@@ -26,7 +26,7 @@ st.set_page_config(
 # =========================
 # 기본 상수
 # =========================
-SHEET_URL_DEFAULT = "https://drive.google.com/file/d/1LQz5Gb_vHji8SKjaJ5hw1ZJQ3z9vUj0r/view?usp=sharing"
+SHEET_URL_DEFAULT = "https://docs.google.com/spreadsheets/d/1FMxN2iS0srEZD2bQ5dlQp2-JaZaXvd24W-glJpqReuo/edit?usp=sharing"
 DB_WORKSHEET_NAME = "DB_배치데이터"
 LOG_WORKSHEET_NAME = "예측결과_log"
 
